f18109670a60dd8506da2fdf437466bbb6d59f3e
TiagoNovaes <tiagousnovaes@gmail.com>
2023-05-11 00:05:58 -0300

    Database connection

---
+++ src/controllers/games.controllers.js
@@ -0,0 +1,11 @@
+import { db } from "../database/database.connection.js";
+
+export async function getGames(req, res) {
+    try {
+        const games = await db.query(`SELECT * FROM games;`);
+        console.table(games.rows);
+        res.send(games.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
