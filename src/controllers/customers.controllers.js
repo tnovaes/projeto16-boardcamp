7ce9aaf5cbe9529dcefc3643ca7782ef7973dc7f
TiagoNovaes <tiagousnovaes@gmail.com>
2023-05-13 15:49:18 -0300

    Fix: Birthday Format (2)

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -4,6 +4,7 @@ import { db } from "../database/database.connection.js";
 export async function getCustomers(req, res) {
     try {
         const customers = await db.query(`SELECT * FROM customers;`);
+        console.table(customers.rows);
 
         res.send(customers.rows);
     } catch (err) {
@@ -18,9 +19,11 @@ export async function insertCustomers(req, res) {
         const cpfVerification = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
         if (cpfVerification.rows[0]) return res.status(409).send("Customer already registered");
 
+        const dbBirthday = formatBirthday(birthday);
+
         await db.query(`
         INSERT INTO customers (name, phone, cpf, birthday) 
-            VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday.slice(0,10)]);
+            VALUES ($1, $2, $3, $4);`, [name, phone, cpf, dbBirthday]);
 
         res.sendStatus(201);
     } catch (err) {
@@ -33,7 +36,7 @@ export async function getCustomersById(req, res) {
 
     try {
         const customers = await db.query(`SELECT * FROM customers WHERE id = $1;`, [id]);
-        if (!customers.rows[0]) return res.status(404).send("Customer not registered")
+        if (!customers.rows[0]) return res.status(404).send("Customer not registered");
 
         res.send(customers.rows[0]);
     } catch (err) {
@@ -49,12 +52,18 @@ export async function updateCustomers(req, res) {
         const cpfVerification = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
         if (cpfVerification.rows[0].id.toString() !== id) return res.status(409).send("CPF already registered");
 
+        const dbBirthday = formatBirthday(birthday);
+
         await db.query(`
         UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 
-            WHERE id = $5;`, [name, phone, cpf, birthday.slice(0,10), id]);
+            WHERE id = $5;`, [name, phone, cpf, dbBirthday, id]);
 
         res.sendStatus(200);
     } catch (err) {
         res.status(500).send(err.message);
     }
+}
+
+function formatBirthday(birthday){
+    return birthday.slice(0,10);
 }
